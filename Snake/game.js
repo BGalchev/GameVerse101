5a6986a0d5d2dc6a87deca06f32d5b2f6592b448
Borislav Galchev <galchevbobi@gmail.com>
2023-05-30 15:25:58 +0300

    Update for the core mechanics of the game.
    These changes enable the snake to grow when it eats an apple and end the game when the snake collides with itself or the walls. The snake's movement speed has been adjusted by introducing a delay between frames, providing a slower gameplay experience.
    
    1. Introduced an array-based representation of the snake with segments storing their position and size.
    2. Modified the updateSnakePosition() function to loop through the snake segments and draw them individually.
    3. Updated the checkCollision() function to include collision detection with walls, the snake itself, and the apple.
    4. Modified the generateRandomApple() function to generate random coordinates within the canvas grid.
    5. Added the growSnake() function to extend the snake by adding a new segment when it eats an apple.
    6. Implemented the gameOver() function to handle game over conditions, showing an alert and resetting the game.
    7. Created the resetGame() function to reset the snake, direction, score, and generate a new random apple.
    8. Adjusted the gameLoop() function to update the snake's position by adding a new head segment and removing the tail segment. Added calls to checkCollision(), updateSnakePosition(), and spawnApples() within the game loop.
    9. Introduced a delay between frames using setTimeout to control the snake's movement speed. The delay can be adjusted to change the game's frame rate.

--- Snake/game.js
+++ Snake/game.js
@@ -1,18 +1,18 @@
 window.addEventListener('load', () => {
     const canvas = document.querySelector('#snake');
     const context = canvas.getContext('2d');
-    const gridSize = 25;
-    const snakeSpeed = 25;
+    const gridSize = 50;
 
     const canvasWidthInBlocks = Math.floor(canvas.width / gridSize);
     const canvasHeightInBlocks = Math.floor(canvas.height / gridSize);
 
-    let snake = { x: 0, y: 0, width: gridSize, height: gridSize };
+    let snake = [{ x: 0, y: 0, width: gridSize, height: gridSize }];
     let direction = 'down';
     let score = 0;
     let apple = { x: 0, y: 0, size: gridSize };
 
     function drawGrid() {
+        // Draw the grid lines on the canvas
         context.strokeStyle = 'black';
 
         for (let x = 0; x < canvasWidthInBlocks; x++) {
@@ -23,20 +23,26 @@ window.addEventListener('load', () => {
     }
 
     function updateSnakePosition() {
+        // Clear the canvas and redraw the grid
         context.clearRect(0, 0, canvas.width, canvas.height);
         drawGrid();
+
+        // Draw each segment of the snake
         context.fillStyle = 'green';
-        context.fillRect(snake.x, snake.y, snake.width, snake.height);
+        for (let i = 0; i < snake.length; i++) {
+            const segment = snake[i];
+            context.fillRect(segment.x, segment.y, segment.width, segment.height);
+        }
     }
 
     function spawnApples() {
-        context.clearRect(apple.x * gridSize, apple.y * gridSize, apple.size, apple.size); // Clear previous apple
+        // Draw the apple on the canvas
         context.fillStyle = 'red';
         context.fillRect(apple.x * gridSize, apple.y * gridSize, apple.size, apple.size);
     }
 
-
     function handleInput(event) {
+        // Update the direction based on user input
         switch (event.key) {
             case 'ArrowUp':
                 if (direction !== 'down')
@@ -56,51 +62,94 @@ window.addEventListener('load', () => {
                 break;
         }
     }
+
     function checkCollision() {
+        const head = snake[0];
 
-        // Convert snake's position to grid units
-        const snakeGridX = Math.floor(snake.x / gridSize);
-        const snakeGridY = Math.floor(snake.y / gridSize);
+        // Check collision with the walls
+        if (
+            head.x < 0 ||
+            head.x + head.width > canvas.width ||
+            head.y < 0 ||
+            head.y + head.height > canvas.height
+        ) {
+            gameOver();
+        }
 
-        if (snakeGridX === apple.x && snakeGridY === apple.y) {
+        // Check collision with itself
+        for (let i = 1; i < snake.length; i++) {
+            const segment = snake[i];
+            if (head.x === segment.x && head.y === segment.y) {
+                gameOver();
+            }
+        }
+
+        // Check collision with the apple
+        if (head.x === apple.x * gridSize && head.y === apple.y * gridSize) {
             // Snake has collided with an apple
             score++;
-            generateRandomApple(); // Generate a new random apple
+            generateRandomApple();
+            growSnake();
         }
     }
 
     function generateRandomApple() {
+        // Generate random coordinates for the apple within the canvas grid
         apple.x = Math.floor(Math.random() * canvasWidthInBlocks);
         apple.y = Math.floor(Math.random() * canvasHeightInBlocks);
-        console.log('spawned')
-        spawnApples(); // Draw the initial apple on the canvas
+    }
 
+    function growSnake() {
+        // Add a new segment to the snake, extending its length
+        const tail = snake[snake.length - 1];
+        snake.push({ x: tail.x, y: tail.y, width: gridSize, height: gridSize });
+    }
+
+    function gameOver() {
+        // Display game over message with the score and reset the game
+        alert(`Game over!\nYour score is ${score}!`);
+        resetGame();
+    }
+
+    function resetGame() {
+        // Reset the snake, direction, score, and generate a new apple
+        snake = [{ x: 0, y: 0, width: gridSize, height: gridSize }];
+        direction = 'down';
+        score = 0;
+        generateRandomApple();
     }
 
     function gameLoop() {
-        const deltaTime = 1.8 / snakeSpeed;
+        const head = snake[0];
+        let newX = head.x;
+        let newY = head.y;
 
-        if (snake.y >= 0 && direction === 'up') {
-            snake.y -= snakeSpeed * deltaTime;
-        } else if (snake.y + gridSize <= canvas.height && direction === 'down') {
-            snake.y += snakeSpeed * deltaTime;
-        } else if (snake.x >= 0 && direction === 'left') {
-            snake.x -= snakeSpeed * deltaTime;
-        } else if (snake.x + gridSize <= canvas.width && direction === 'right') {
-            snake.x += snakeSpeed * deltaTime;
+        // Update the head position based on the current direction
+        if (direction === 'up') {
+            newY -= gridSize;
+        } else if (direction === 'down') {
+            newY += gridSize;
+        } else if (direction === 'left') {
+            newX -= gridSize;
+        } else if (direction === 'right') {
+            newX += gridSize;
         }
 
+        const newHead = { x: newX, y: newY, width: gridSize, height: gridSize };
+
+        // Update the snake by adding a new head segment and removing the tail segment
+        snake.unshift(newHead);
+        snake.pop();
+
         updateSnakePosition();
+        checkCollision();
+        spawnApples();
 
-        spawnApples(); // Draw the initial apple on the canvas
-
-        checkCollision(); // Check for collision with apple
-        requestAnimationFrame(gameLoop);
+        setTimeout(gameLoop, 180); // Introduce a delay of 180 milliseconds (18 frames per second)
     }
 
     document.addEventListener('keydown', handleInput);
 
     generateRandomApple(); // Generate the initial random apple
-
-    gameLoop();
+    gameLoop(); // Start the game loop
 });
