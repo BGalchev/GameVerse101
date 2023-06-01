d7c98b445c3f02b1c717982e115571cbdef5723c
Borislav Galchev <galchevbobi@gmail.com>
2023-06-01 15:56:38 +0300

    Fixed pacing bug
    The pacing was increasing as the game goes on and making it impossible to play after few minutes.
    And moved some functions around.

--- Snake/game.js
+++ Snake/game.js
@@ -12,8 +12,7 @@ window.addEventListener('load', () => {
   let score = 0;
   let apple = { x: 0, y: 0, size: gridSize };
   let isGameOver = false;
-  let gameTimer;
-
+  let gameTimer = null; 
 
   function drawGrid() {
     // Draw the grid lines on the canvas
@@ -26,116 +25,7 @@ window.addEventListener('load', () => {
     }
   }
 
-  function updateSnakePosition() {
-    // Clear the canvas and redraw the grid
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawGrid();
-
-    // Draw each segment of the snake
-    context.fillStyle = 'green';
-    for (let i = 0; i < snake.length; i++) {
-      const segment = snake[i];
-      context.fillRect(segment.x, segment.y, segment.width, segment.height);
-    }
-  }
-
-  function spawnApples() {
-    // Draw the apple on the canvas
-    context.fillStyle = 'red';
-    context.fillRect(apple.x * gridSize, apple.y * gridSize, apple.size, apple.size);
-  }
-
-  function handleInput(event) {
-    // Restart the game if it's over and any key is pressed
-    if (isGameOver) {
-      restartGame()
-      return;
-    }
-
-    // Update the next direction based on user input
-    switch (event.key) {
-      case 'ArrowUp':
-        if (direction !== 'down')
-          nextDirection = 'up';
-        break;
-      case 'ArrowDown':
-        if (direction !== 'up')
-          nextDirection = 'down';
-        break;
-      case 'ArrowLeft':
-        if (direction !== 'right')
-          nextDirection = 'left';
-        break;
-      case 'ArrowRight':
-        if (direction !== 'left')
-          nextDirection = 'right';
-        break;
-    }
-  }
-
-
-  function checkCollision() {
-    const head = snake[0];
-
-    // Check collision with the walls
-    if (
-      head.x < 0 ||
-      head.x + head.width > canvas.width ||
-      head.y < 0 ||
-      head.y + head.height > canvas.height
-    ) {
-      gameOver();
-    }
-
-    // Check collision with itself (excluding the head)
-    for (let i = 1; i < snake.length; i++) {
-      const segment = snake[i];
-      if (head.x === segment.x && head.y === segment.y) {
-        gameOver();
-      }
-    }
-
-    // Check collision with the apple
-    if (head.x === apple.x * gridSize && head.y === apple.y * gridSize) {
-      // Snake has collided with an apple
-      score++;
-      growSnake();
-      generateRandomApple();
-      return;
-    }
-
-    // Check if the snake has collided with its own body in a circular path
-    const tail = snake.slice(1); // Exclude the head from the collision check
-    const hasCircularCollision = tail.some((segment) => head.x === segment.x && head.y === segment.y);
-    if (hasCircularCollision) {
-      gameOver();
-    }
-  }
-
-  function generateRandomApple() {
-    let appleX, appleY;
-    let isSnakeOverlap = true;
-
-    // Keep generating random coordinates until a valid spot is found
-    while (isSnakeOverlap) {
-      appleX = Math.floor(Math.random() * canvasWidthInBlocks);
-      appleY = Math.floor(Math.random() * canvasHeightInBlocks);
-
-      // Check if the generated apple coordinates overlap with the snake's body
-      isSnakeOverlap = snake.some((segment) => segment.x === appleX && segment.y === appleY);
-    }
-
-    apple.x = appleX;
-    apple.y = appleY;
-  }
-
-  function growSnake() {
-    // Add a new segment to the snake, extending its length
-    const tail = snake[snake.length - 1];
-    snake.push({ x: tail.x, y: tail.y, width: gridSize, height: gridSize });
-  }
-
-  function gameOver() {
+ function gameOver() {
     // Set the game over flag and redraw the game over message
     isGameOver = true;
     context.font = '40px Arial';
@@ -163,12 +53,10 @@ window.addEventListener('load', () => {
       score = 0;
       generateRandomApple();
       isGameOver = false;
-      gameLoop(); // Start the game loop
+     gameLoop(); // Start the game loop
     }
   }
-
-
-  function gameLoop() {
+function gameLoop() {
     const head = snake[0];
     let newX = head.x;
     let newY = head.y;
@@ -214,13 +102,108 @@ window.addEventListener('load', () => {
 
     gameTimer = setTimeout(gameLoop, 200); // Introduce a delay of 200 milliseconds (5 frames per second)
   }
-
-  canvas.addEventListener('click', () => {
-    if (isGameOver) {
-      restartGame();
-      gameLoop();
+function handleInput(event) {
+    // Update the next direction based on user input
+    switch (event.key) {
+      case 'ArrowUp':
+        if (direction !== 'down')
+          nextDirection = 'up';
+        break;
+      case 'ArrowDown':
+        if (direction !== 'up')
+          nextDirection = 'down';
+        break;
+      case 'ArrowLeft':
+        if (direction !== 'right')
+          nextDirection = 'left';
+        break;
+      case 'ArrowRight':
+        if (direction !== 'left')
+          nextDirection = 'right';
+        break;
     }
-  });
+  }
+
+  function updateSnakePosition() {
+    // Clear the canvas and redraw the grid
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
+
+    // Draw each segment of the snake
+    context.fillStyle = 'green';
+    for (let i = 0; i < snake.length; i++) {
+      const segment = snake[i];
+      context.fillRect(segment.x, segment.y, segment.width, segment.height);
+    }
+  }
+
+  function spawnApples() {
+    // Draw the apple on the canvas
+    context.fillStyle = 'red';
+    context.fillRect(apple.x * gridSize, apple.y * gridSize, apple.size, apple.size);
+  }
+ 
+  function generateRandomApple() {
+    let appleX, appleY;
+    let isSnakeOverlap = true;
+
+    // Keep generating random coordinates until a valid spot is found
+    while (isSnakeOverlap) {
+      appleX = Math.floor(Math.random() * canvasWidthInBlocks);
+      appleY = Math.floor(Math.random() * canvasHeightInBlocks);
+
+      // Check if the generated apple coordinates overlap with the snake's body
+      isSnakeOverlap = snake.some((segment) => segment.x === appleX && segment.y === appleY);
+    }
+
+    apple.x = appleX;
+    apple.y = appleY;
+  } 
+
+  function checkCollision() {
+    const head = snake[0];
+
+    // Check collision with the walls
+    if (
+      head.x < 0 ||
+      head.x + head.width > canvas.width ||
+      head.y < 0 ||
+      head.y + head.height > canvas.height
+    ) {
+      gameOver();
+    }
+
+    // Check collision with itself (excluding the head)
+    for (let i = 1; i < snake.length; i++) {
+      const segment = snake[i];
+      if (head.x === segment.x && head.y === segment.y) {
+        gameOver();
+      }
+    }
+
+    // Check collision with the apple
+    if (head.x === apple.x * gridSize && head.y === apple.y * gridSize) {
+      // Snake has collided with an apple
+      score++;
+      growSnake();
+      generateRandomApple();
+      return;
+    }
+
+    // Check if the snake has collided with its own body in a circular path
+    const tail = snake.slice(1); // Exclude the head from the collision check
+    const hasCircularCollision = tail.some((segment) => head.x === segment.x && head.y === segment.y);
+    if (hasCircularCollision) {
+      gameOver();
+    }
+  }
+
+
+  function growSnake() {
+    // Add a new segment to the snake, extending its length
+    const tail = snake[snake.length - 1];
+    snake.push({ x: tail.x, y: tail.y, width: gridSize, height: gridSize });
+  }
 
   document.addEventListener('keydown', handleInput);
 
