3ec0b0a4df7bbf94e5c6a07b33e5734a5bbbd1fe
Borislav Galchev <galchevbobi@gmail.com>
2023-06-01 22:09:44 +0300

    Finishing Touches...
    Implemented changes to improve the game functionality and user experience.
    
    - Removed the unnecessary grid system that caused redrawing of the entire canvas on every snake movement, resulting in improved performance.
    - Adjusted the canvas size to 8x8 using a grid of 75px, providing a more visually pleasing and balanced gameplay environment.
    - Updated the winning and losing screens to enhance the overall game experience and provide clearer feedback to the player.
    - Attempted to fix the issue with random apple generation, where the apple was occasionally appearing in a spot occupied by the snake due to the redrawing process. However, due to current limitations in programming skills, the issue remains unresolved.
    
    These changes aim to create a smoother and more enjoyable gameplay experience for the users. Further improvements and bug fixes will be addressed in future updates.

--- Snake/game.js
+++ Snake/game.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
   const canvas = document.querySelector('#snake');
   const context = canvas.getContext('2d');
-  const gridSize = 50;
+  const gridSize = 75;
 
   const canvasWidthInBlocks = Math.floor(canvas.width / gridSize);
   const canvasHeightInBlocks = Math.floor(canvas.height / gridSize);
@@ -12,56 +12,63 @@ window.addEventListener('load', () => {
   let score = 0;
   let apple = { x: 0, y: 0, size: gridSize };
   let isGameOver = false;
-  let gameTimer = null; 
+  let gameTimer = null;
+  let isGameWon = false;
 
-  function updateGameScore(score){
+  function updateGameScore(score) {
     let scoreElement = document.getElementById('game-score');
-    scoreElement.textContent =`Apples eaten: ${score}`
+    scoreElement.textContent = `Apples eaten: ${score}`;
   }
 
-  function drawGrid() {
-    // Draw the grid lines on the canvas
-    context.strokeStyle = 'black';
-
-    for (let x = 0; x < canvasWidthInBlocks; x++) {
-      for (let y = 0; y < canvasHeightInBlocks; y++) {
-        context.strokeRect(x * gridSize, y * gridSize, gridSize, gridSize);
-      }
+   function updateSnakePosition() {
+    // Clear the canvas and redraw the grid
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Draw each segment of the snake
+    context.fillStyle = 'green';
+    for (let i = 0; i < snake.length; i++) {
+      const segment = snake[i];
+      context.fillRect(segment.x, segment.y, segment.width, segment.height);
     }
   }
 
-  function gameWon(){
-    isGameOver = true;
-    context.font = '40px Arial';
-    context.fillStyle = 'white';
-    context.fillText(`You WON! Score: ${score}`, canvas.width / 2 - 200, canvas.height / 2);
-    context.font = '20px Arial';
-    context.fillText('Click any key to play again!', canvas.width / 2 - 100, canvas.height / 2 + 30);
+function gameOver() {
+  isGameOver = true;
+  if(score == 63){
+      isGameWon = true;
+      canvas.style.backgroundColor = 'gold';
+    } else{
+    canvas.style.backgroundColor = 'red';
+    }
+  // Clear the canvas
+  context.clearRect(0, 0, canvas.width, canvas.height);
+ 
+  // Set the text properties
+  context.font = '40px Arial';
+  context.fillStyle = 'white';
+  context.textAlign = 'center';
 
-    // Listen for any keydown event to restart the game
-    document.addEventListener('keydown', restartGame);
+  if (isGameWon) {
+    // Display "You WON" message
+    context.fillText(`You WON! Score: ${score}`, canvas.width / 2, canvas.height / 2);
+    context.font = '20px Arial';
+    context.fillText('Thanks For Playing!', canvas.width / 2, canvas.height / 2 + 30);
+  } else {
+    // Display "Game Over" message
+    context.fillText(`Game Over! Score: ${score}`, canvas.width / 2, canvas.height / 2);
+    context.font = '20px Arial';
+    context.fillText('Click any key to restart', canvas.width / 2, canvas.height / 2 + 30);
   }
 
-
-
- function gameOver() {
-    // Set the game over flag and redraw the game over message
-    isGameOver = true;
-    context.font = '40px Arial';
-    context.fillStyle = 'white';
-    context.fillText(`Game Over! Score: ${score}`, canvas.width / 2 - 200, canvas.height / 2);
-    context.font = '20px Arial';
-    context.fillText('Click any key to restart', canvas.width / 2 - 100, canvas.height / 2 + 30);
-
     // Listen for any keydown event to restart the game
     document.addEventListener('keydown', restartGame);
-
   }
 
   function restartGame(event) {
-    clearTimeout(gameTimer); 
+    clearTimeout(gameTimer);
     score = 0;
     updateGameScore(score);
+    canvas.style.backgroundColor = 'grey'
     // Check if the game is over and a key is pressed
     if (isGameOver) {
       // Remove the event listener
@@ -74,10 +81,11 @@ window.addEventListener('load', () => {
       score = 0;
       generateRandomApple();
       isGameOver = false;
-     gameLoop(); // Start the game loop
+      gameLoop(); // Start the game loop
     }
   }
-function gameLoop() {
+
+  function gameLoop() {
     const head = snake[0];
     let newX = head.x;
     let newY = head.y;
@@ -104,11 +112,8 @@ function gameLoop() {
     }
 
     const newHead = { x: newX, y: newY, width: gridSize, height: gridSize };
-
-    // Check if the new position is valid (not colliding with the snake's body)
     const isPositionValid = !snake.some((segment, index) => index !== 0 && segment.x === newX && segment.y === newY);
 
-    // Update the snake only if the new position is valid
     if (isPositionValid) {
       snake.unshift(newHead);
       snake.pop();
@@ -116,14 +121,15 @@ function gameLoop() {
       gameOver();
       return;
     }
-
+    
     updateSnakePosition();
     checkCollision();
     spawnApples();
 
     gameTimer = setTimeout(gameLoop, 200); // Introduce a delay of 200 milliseconds (5 frames per second)
   }
-function handleInput(event) {
+
+  function handleInput(event) {
     // Update the next direction based on user input
     switch (event.key) {
       case 'ArrowUp':
@@ -145,41 +151,43 @@ function handleInput(event) {
     }
   }
 
-  function updateSnakePosition() {
-    // Clear the canvas and redraw the grid
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawGrid();
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-    // Draw each segment of the snake
-    context.fillStyle = 'green';
-    for (let i = 0; i < snake.length; i++) {
-      const segment = snake[i];
-      context.fillRect(segment.x, segment.y, segment.width, segment.height);
+function generateRandomApple() {
+  const occupiedPositions = new Set(snake.map((segment) => `${segment.x},${segment.y}`));
+
+  // Get all the valid positions where the apple can be placed
+  const validPositions = [];
+  for (let x = 0; x < canvasWidthInBlocks; x++) {
+    for (let y = 0; y < canvasHeightInBlocks; y++) {
+      const positionKey = `${x},${y}`;
+      if (!occupiedPositions.has(positionKey)) {
+        validPositions.push({ x, y });
+      }
     }
   }
+
+  // Randomly select one of the valid positions
+  const randomIndex = getRandomInt(0, validPositions.length - 1);
+  const { x, y } = validPositions[randomIndex];
+
+  apple.x = x;
+  apple.y = y;
+
+  spawnApples();
+}
+
 
   function spawnApples() {
     // Draw the apple on the canvas
-    context.fillStyle = 'red';
-    context.fillRect(apple.x * gridSize, apple.y * gridSize, apple.size, apple.size);
+    const appleArt = new Image();
+    appleArt.src = 'PixelArt/apple.png';
+    context.drawImage(appleArt, apple.x * gridSize, apple.y * gridSize, apple.size, apple.size);
   }
- 
-  function generateRandomApple() {
-    let appleX, appleY;
-    let isSnakeOverlap = true;
-
-    // Keep generating random coordinates until a valid spot is found
-    while (isSnakeOverlap) {
-      appleX = Math.floor(Math.random() * canvasWidthInBlocks);
-      appleY = Math.floor(Math.random() * canvasHeightInBlocks);
-
-      // Check if the generated apple coordinates overlap with the snake's body
-      isSnakeOverlap = snake.some((segment) => segment.x === appleX && segment.y === appleY);
-    }
-
-    apple.x = appleX;
-    apple.y = appleY;
-  } 
 
   function checkCollision() {
     const head = snake[0];
@@ -199,41 +207,29 @@ function handleInput(event) {
       const segment = snake[i];
       if (head.x === segment.x && head.y === segment.y) {
         gameOver();
+        return;
       }
     }
 
     // Check collision with the apple
     if (head.x === apple.x * gridSize && head.y === apple.y * gridSize) {
-      // Snake has collided with an apple
       score++;
-     updateGameScore(score); 
-      growSnake();
+      updateGameScore(score);
+
+      // Add a new segment to the snake
+      const tail = snake[snake.length - 1];
+      const newSegment = { x: tail.x, y: tail.y, width: gridSize, height: gridSize };
+      snake.push(newSegment);
+
+      // Generate a new apple
       generateRandomApple();
-      return;
     }
-
-    // Check if the snake has collided with its own body in a circular path
-    const tail = snake.slice(1); // Exclude the head from the collision check
-    const hasCircularCollision = tail.some((segment) => head.x === segment.x && head.y === segment.y);
-    if (hasCircularCollision) {
-      gameOver();
-    }
-  }
-
-
-  function growSnake() {
-    if(snake.length < 143){
-   // Add a new segment to the snake, extending its length
-    const tail = snake[snake.length - 1];
-    snake.push({ x: tail.x, y: tail.y, width: gridSize, height: gridSize });
-    }else{
-      gameWon()
-    }
- 
   }
 
   document.addEventListener('keydown', handleInput);
 
-  generateRandomApple(); // Generate the initial random apple
-  gameLoop(); // Start the game loop
+  // Start the game
+  generateRandomApple();
+  gameLoop();
 });
+
